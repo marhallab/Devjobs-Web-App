e11751d6f221007c585d097dcdaf887c3fe7893e
med_amine_rhallab <1832012@cegeplimoilou.ca>
2021-09-22 15:58:23 -0400

    Making sass work

--- devjobs-web-app/src/components/JobsList.vue
+++ devjobs-web-app/src/components/JobsList.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="jobsList">
-    <div v-for="job in jobs" :key="job.id">
+    <div class="jobsList-jobs" v-for="job in jobs" :key="job.id">
       <p>{{ job.title }}</p>
     </div>
   </div>
 </template>
+
+<style lang="scss" scoped>
+
+.jobsList{
+  background-color: red;
+
+  &-jobs{
+    color: $violet ;
+  }
+}
+</style>
 
 <script>
 import gql from "graphql-tag";
